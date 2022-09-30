de1473f039f62b78e8d054da68b58f37f9059dce
canjalal <vincent.shuali@gmail.com>
2022-09-30 12:24:12 -0700

    Add online events to eventpanel on GroupShow

--- frontend/src/components/EventPages/eventpanel.jsx
+++ frontend/src/components/EventPages/eventpanel.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { GoLocation } from 'react-icons/go'
+import { GrVideo } from 'react-icons/gr';
 
 const EventPanel = ({ data }) => {
   return (
     <div className="event-panel">
         <p className="date-label">{new Date(data.dateTime).toDateString()} {new Date(data.dateTime).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'})}</p>
         <h2 className="event-panel-title">{data.title}</h2>
-        <div>{data.venue ? <><GoLocation />  {data.venue} </> : ""}</div>
+        <div>{data.online === "yes" ? <><GrVideo />{"  Online event" }</> : data.venue ? <><GoLocation />  {data.venue} </> : ""}</div>
         <div className="event-panel-description">{data.description}</div>
     </div>
   )
