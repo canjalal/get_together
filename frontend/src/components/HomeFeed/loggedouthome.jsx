310b6141c0c3e134e830276f35cc71bdbff6cb83
canjalal <vincent.shuali@gmail.com>
2022-10-12 16:53:37 -0700

    Restyle home hero image for mobile

--- frontend/src/components/HomeFeed/loggedouthome.jsx
+++ frontend/src/components/HomeFeed/loggedouthome.jsx
@@ -7,10 +7,10 @@ const LoggedOutHome = () => {
         <div id="intro-row">
             <div id="intro-text">
                 <h1 className="big-title">
-                    Celebrating 20 years of real connections on Meetup
+                    Get-Together: Celebrating 20 years of real connections on Meetup
                 </h1>
                 <p>
-                Whatever you’re looking to do this year, Meetup can help. For 20 years, people have turned to Meetup to meet people, make friends, find support, grow a business, and explore their interests. Thousands of events are happening every day—join the fun.
+                For 20 years, people have turned to Meetup to meet people, make friends, find support, grow a business, and explore their interests. Get-Together is a tribute to their product.
                 </p>
             </div>
             <div id="intro-image">
