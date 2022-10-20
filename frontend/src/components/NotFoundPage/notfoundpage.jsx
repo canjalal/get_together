13c103dd667cf1b01c7f5142353dd7ecfba01386
canjalal <vincent.shuali@gmail.com>
2022-10-19 17:59:00 -0700

    Not found error should not display if there is a groupId

--- frontend/src/components/NotFoundPage/notfoundpage.jsx
+++ frontend/src/components/NotFoundPage/notfoundpage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 import './notfound.css';
 
 const NotFoundPage = () => {
-  return (
+
+  const { groupId } = useParams();
+  return !groupId && (
     <div className="not-found">
         <h1>Sorry, the group you're looking for doesn't exist.</h1>     
         <p>
