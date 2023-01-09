564ccd10632341358d76c755269ca75d9fd3ada5
canjalal <vincent.shuali@gmail.com>
2023-01-08 17:01:15 -0800

    EventSubmitBar component split off from EventNewForm

--- frontend/src/components/EventPages/EventNewForm.jsx
+++ frontend/src/components/EventPages/EventNewForm.jsx
@@ -6,8 +6,8 @@ import { createEvent, patchEvent } from '../../store/events';
 import { fetchGroup, getGroup } from '../../store/groups';
 import { getCurrentUser } from '../../store/session'
 import { getUser } from '../../store/users';
-import { EventErrors } from './EventErrors';
 import './eventform.css';
+import { EventSubmitBar } from './EventSubmitBar';
 
 const EventNewForm = ({oldEvent}) => {
 
@@ -151,36 +151,13 @@ const EventNewForm = ({oldEvent}) => {
                     Location
                     <br />
                     <input type="checkbox" id="is-online-checkbox" value={isOnline} onChange={(e) => setIsOnline(e.target.checked ? true : false)} /> Make this an online event
-
                 </label>
                 <label>
                     Add venue
                     <input type="text" className="high-inputs" placeholder="Add a location" value={venue} onChange={(e) => setVenue(e.target.value)} />
                 </label>
             </div>
-            <div id="event-submit-bar">
-                <div>
-                    <button className="back-button" onClick={() => navigate(`/groups/${groupId}`)}>Cancel</button>
-                </div>
-                <div>
-                    <button className="standard-button" id="next-button" onClick={(e) => submitEvent(e).catch(
-                    async (res) => { // not correctly catching error 422, re-renders a blank page (not what we want)
-                    let data;
-                    try {
-                        // .clone() essentially allows you to read the response body twice
-                        data = await res.clone().json();
-                    } catch {
-                        data = await res.text(); // Will hit this case if, e.g., server is down
-                    }
-                    if (data?.errors) setErrors(data.errors);
-                    else if (data) setErrors([data]);
-                    else setErrors([res.statusText]);
-                    }
-                )}>{oldEvent.method ? "Publish" : "Save Changes"}</button>
-                {errors.length > 0 && <EventErrors errors={errors} />
-                }
-                </div>
-            </div>
+            <EventSubmitBar submitEvent={submitEvent} groupId={groupId} oldEvent={oldEvent} />
         </form>
     </div>
   )
--- frontend/src/components/EventPages/EventSubmitBar.jsx
+++ frontend/src/components/EventPages/EventSubmitBar.jsx
@@ -1,25 +1,36 @@
 import { EventErrors } from "./EventErrors";
 
-<div id="event-submit-bar">
-                <div>
-                    <button className="back-button" onClick={() => navigate(`/groups/${groupId}`)}>Cancel</button>
-                </div>
-                <div>
-                    <button className="standard-button" id="next-button" onClick={(e) => submitEvent(e).catch(
-                    async (res) => { // not correctly catching error 422, re-renders a blank page (not what we want)
-                    let data;
-                    try {
-                        // .clone() essentially allows you to read the response body twice
-                        data = await res.clone().json();
-                    } catch {
-                        data = await res.text(); // Will hit this case if, e.g., server is down
-                    }
-                    if (data?.errors) setErrors(data.errors);
-                    else if (data) setErrors([data]);
-                    else setErrors([res.statusText]);
-                    }
-                )}>{oldEvent.method ? "Publish" : "Save Changes"}</button>
-                {errors.length > 0 && <EventErrors errors={errors} />
-                }
-                </div>
-            </div>
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+
+export const EventSubmitBar = ({submitEvent, groupId, oldEvent}) => {
+
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState([]);
+
+  return (
+    <div id="event-submit-bar">
+        <div>
+            <button className="back-button" onClick={() => navigate(`/groups/${groupId}`)}>Cancel</button>
+        </div>
+        <div>
+            <button className="standard-button" id="next-button" onClick={(e) => submitEvent(e).catch(
+            async (res) => { // not correctly catching error 422, re-renders a blank page (not what we want)
+            let data;
+            try {
+                // .clone() essentially allows you to read the response body twice
+                data = await res.clone().json();
+            } catch {
+                data = await res.text(); // Will hit this case if, e.g., server is down
+            }
+            if (data?.errors) setErrors(data.errors);
+            else if (data) setErrors([data]);
+            else setErrors([res.statusText]);
+            }
+        )}>{oldEvent.method ? "Publish" : "Save Changes"}</button>
+        {errors.length > 0 && <EventErrors errors={errors} />
+        }
+        </div>
+    </div>
+  )
+}
